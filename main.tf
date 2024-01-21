13d20f1fdc027fa1100e3f516b655ce207245a8d
Vanderson Nunes <vann.nunes@gmail.com>
2024-01-20 22:02:30 -0300

    :qAdding producao service

--- main.tf
+++ main.tf
@@ -6,6 +6,11 @@ data "aws_lb" "auth_lb" {
   name = var.auth_nlb_name
 }
 
+data "aws_lb" "producao_lb" {
+  name = var.producao_nlb_name
+}
+
+
 data "aws_lambda_function" "lambda_authorizer" {
   function_name = var.lambda_authorizer_name
 }
@@ -68,7 +73,7 @@ resource "aws_api_gateway_authorizer" "tech_challenge_authorizer" {
   identity_source        = "method.request.header.Authorization"
 }
 
-# VPC links
+#---- VPC links ----
 
 resource "aws_api_gateway_vpc_link" "auth_vpc_link" {
   name        = "tech-challenge-auth-vpc-link"
@@ -80,7 +85,12 @@ resource "aws_api_gateway_vpc_link" "lanchonete_vpc_link" {
   target_arns = [data.aws_lb.lanchonete_lb.arn]
 }
 
-# Auth API configuration
+resource "aws_api_gateway_vpc_link" "producao_vpc_link" {
+  name        = "tech-challenge-producao-vpc-link"
+  target_arns = [data.aws_lb.producao_lb.arn]
+}
+
+#---- Auth API configuration ----
 
 resource "aws_api_gateway_resource" "auth_resource" {
   rest_api_id = aws_api_gateway_rest_api.tech_challenge_gw.id
@@ -131,7 +141,7 @@ resource "aws_api_gateway_integration" "auth_integration" {
   depends_on = [ aws_api_gateway_method.auth_any ]
 }
 
-# Lanchonete API configuration
+#---- Lanchonete API configuration ----
 
 resource "aws_api_gateway_resource" "lanchonete_resource" {
   rest_api_id = aws_api_gateway_rest_api.tech_challenge_gw.id
@@ -185,6 +195,59 @@ resource "aws_api_gateway_integration" "lanchonete_integration" {
   depends_on = [ aws_api_gateway_method.lanchonete_any ]
 }
 
+#---- Producao API configuration ----
+
+resource "aws_api_gateway_resource" "producao_resource" {
+  rest_api_id = aws_api_gateway_rest_api.tech_challenge_gw.id
+  parent_id   = aws_api_gateway_rest_api.tech_challenge_gw.root_resource_id
+  path_part   = "producao"
+
+  depends_on = [ aws_api_gateway_rest_api.tech_challenge_gw ]
+}
+
+resource "aws_api_gateway_resource" "producao_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.tech_challenge_gw.id
+  parent_id   = aws_api_gateway_resource.producao_resource.id
+  path_part   = "{proxy+}"
+
+  depends_on = [ aws_api_gateway_resource.producao_resource ]
+}
+
+resource "aws_api_gateway_method" "producao_any"  {
+  rest_api_id   = aws_api_gateway_rest_api.tech_challenge_gw.id
+  resource_id   = aws_api_gateway_resource.producao_proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+
+  depends_on = [ aws_api_gateway_resource.producao_proxy ]
+}
+
+resource "aws_api_gateway_integration" "producao_integration" {
+
+  http_method = aws_api_gateway_method.producao_any.http_method
+  resource_id = aws_api_gateway_resource.producao_proxy.id
+  rest_api_id = aws_api_gateway_rest_api.tech_challenge_gw.id
+
+  type                    = "HTTP_PROXY"  
+  integration_http_method = "ANY"
+  uri                     = "http://${data.aws_lb.producao_lb.dns_name}/{proxy}"
+
+  cache_key_parameters = ["method.request.path.proxy"]
+
+  timeout_milliseconds = 29000
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+  
+  connection_type = "VPC_LINK"
+  connection_id   = aws_api_gateway_vpc_link.producao_vpc_link.id
+
+  depends_on = [ aws_api_gateway_method.producao_any ]
+}
+
 # Api Gateway Deployment
 
 resource "aws_api_gateway_deployment" "dev_stage" {
@@ -197,7 +260,8 @@ resource "aws_api_gateway_deployment" "dev_stage" {
 
   depends_on = [
     aws_api_gateway_integration.lanchonete_integration,
-    aws_api_gateway_integration.auth_integration
+    aws_api_gateway_integration.auth_integration,
+    aws_api_gateway_integration.producao_integration
   ]
 }
 
